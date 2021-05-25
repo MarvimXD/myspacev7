1496e80031ca9ed1f01762dc81d092670eae0424
Victor <mxd46934@gmail.com>
2021-05-24 23:41:27 -0300

    pag teste

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ function Home() {
             <h1>MySpaceTO v7 React.js // Next.js - EM BREVE</h1>
             <hr/>
             <h2>by: Victor Beserra</h2>
+            <a href="teste.html">Clique aqui</a>
         </div>
 
     )
