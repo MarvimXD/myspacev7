eb98f47b91d16eb64f0ad6656bd8b5a4404ce572
Victor <mxd46934@gmail.com>
2021-05-24 22:07:52 -0300

    nova home

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,13 @@
-import { useState } from 'react';
 
 function Home() {
+   
     return (
         <div>
-            <h1>MySpaceTO V7</h1>
-            <Contador />
+            <h1>MySpaceTO v7 React.js // Next.js - EM BREVE</h1>
+            <hr/>
+            <h2>by: Victor Beserra</h2>
         </div>
+
     )
-}
-
-function Contador() {
-    const [contador, setContador] = useState(1);
-
-    function adicionarContador() {
-        setContador(contador+1);
     }
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-    )
-}
-
 export default Home
